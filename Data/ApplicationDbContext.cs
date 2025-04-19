0a5adf2790b67c167e64056a69b97bb3d6de307f
A.mohajeri2015 <96079905+amirhosein2015@users.noreply.github.com>
2025-04-19 19:48:19 +0330

    Archive Project 01

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using TodoApp.Models;
+
+namespace TodoApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Todo> Todos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            // مثال تنظیمات اضافی برای مدل‌ها (اختیاری)
+            // تنظیم کلید خارجی برای Todo
+            builder.Entity<Todo>()
+                .HasOne(t => t.User)
+                .WithMany()
+                .HasForeignKey(t => t.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
